f6311086a011802add77e3544a2b51d365b8c796
Jon Reid <jon.reid@mac.com>
2015-03-15 15:19:58 -0700

    Use TestingAppDelegate when running tests

---
+++ MarvelBrowser/TestingAppDelegate.h
@@ -0,0 +1,10 @@
+//  TDD sample app MarvelBrowser by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2015 Jonathan M. Reid. See LICENSE.txt
+
+#import <UIKit/UIKit.h>
+
+@interface TestingAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
+
+@end
--- MarvelBrowser/main.m
+++ MarvelBrowser/main.m
@@ -3,10 +3,13 @@
 
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
+#import "TestingAppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        BOOL isRunningTests = NSClassFromString(@"XCTestCase") != nil;
+        Class appDelegateClass = isRunningTests ? [TestingAppDelegate class] : [AppDelegate class];
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass(appDelegateClass));
     }
 }
