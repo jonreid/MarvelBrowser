07fcfa55345cd9c3eaf3858353cb447e3b99ab2a
Jon Reid <jon@qualitycoding.org>
2015-07-08 07:54:41 -0700

    Tests pass, but MD5Block still needs a default block

--- MarvelBrowser/QCOMarvelAuthentication.m
+++ MarvelBrowser/QCOMarvelAuthentication.m
@@ -33,7 +33,7 @@
 - (NSString *)URLParameters
 {
     return [NSString stringWithFormat:@"&ts=%@&apikey=%@&hash=%@",
-                    self.timestamp, self.publicKey, [self MD5OfString:[self timestampedKeys]]
+                    self.timestamp, self.publicKey, self.MD5Block([self timestampedKeys])
     ];
 }
 
