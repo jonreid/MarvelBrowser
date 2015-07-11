3e2735c4704d1aa9a1eae13a1751fa9f428d3dee
Jon Reid <jon@qualitycoding.org>
2015-07-10 20:18:09 -0700

    Create AcceptanceTests target

---
+++ AcceptanceTests/AcceptanceTests.m
@@ -0,0 +1,13 @@
+#import <XCTest/XCTest.h>
+
+@interface AcceptanceTests : XCTestCase
+@end
+
+@implementation AcceptanceTests
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTFail(@"acceptance");
+}
+
+@end
