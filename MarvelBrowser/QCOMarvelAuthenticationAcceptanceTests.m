ac6897f33d189e18670dfb7fe34203303cff08c4
Jon Reid <jon@qualitycoding.org>
2015-07-10 20:40:39 -0700

    Acceptance test stub for authentication

---
+++ MarvelBrowser/QCOMarvelAuthenticationAcceptanceTests.m
@@ -0,0 +1,19 @@
+//  TDD sample app MarvelBrowser by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2015 Jonathan M. Reid. See LICENSE.txt
+
+#import "QCOMarvelAuthentication.h"
+
+#import <XCTest/XCTest.h>
+
+
+@interface QCOMarvelAuthenticationAcceptanceTests : XCTestCase
+@end
+
+@implementation QCOMarvelAuthenticationAcceptanceTests
+
+- (void)testExample
+{
+    XCTFail(@"Unit tests are not implemented yet in QCOMarvelAuthenticationAcceptanceTests");
+}
+
+@end
