18197a7803383f88872cf33e05e4a0473e77183c
Jon Reid <jon@qualitycoding.org>
2015-07-10 22:48:08 -0700

    True acceptance test for Marvel authentication

--- MarvelBrowser/QCOMarvelAuthenticationAcceptanceTests.m
+++ MarvelBrowser/QCOMarvelAuthenticationAcceptanceTests.m
@@ -5,15 +5,33 @@
 
 #import <XCTest/XCTest.h>
 
+#define HC_SHORTHAND
+#import <OCHamcrest/OCHamcrest.h>
+
 
 @interface QCOMarvelAuthenticationAcceptanceTests : XCTestCase
 @end
 
 @implementation QCOMarvelAuthenticationAcceptanceTests
 
-- (void)testExample
+- (void)testCallToMarvel_ShouldGetStatus200OK
 {
-    XCTFail(@"Unit tests are not implemented yet in QCOMarvelAuthenticationAcceptanceTests");
+    QCOMarvelAuthentication *sut = [[QCOMarvelAuthentication alloc] init];
+    NSString *URLString = [@"http://gateway.marvel.com/v1/public/characters?nameStartsWith=Spider" stringByAppendingString:[sut URLParameters]];
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig
+                                                          delegate:nil
+                                                     delegateQueue:nil];
+
+    __block NSHTTPURLResponse *httpResponse;
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:url
+                                            completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                                                httpResponse = (NSHTTPURLResponse *)response;
+                                            }];
+    [dataTask resume];
+
+    assertThatAfter(5, futureValueOf(@(httpResponse.statusCode)), is(@200));
 }
 
 @end
--- Podfile
+++ Podfile
@@ -1,3 +1,7 @@
 target :MarvelBrowserTests, :exclusive => true do
   pod 'OCHamcrest', '~> 4.0'
 end
+
+target :AcceptanceTests, :exclusive => true do
+  pod 'OCHamcrest', '~> 4.0'
+end
