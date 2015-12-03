85b825a44a8e34f76099baa485e5ce690e0539dc
Jon Reid <jon@qualitycoding.org>
2015-12-02 21:19:22 -0800

    AppCode: Create new class 'QCOMarvelService'

---
+++ MarvelBrowser/QCOMarvelService.m
@@ -0,0 +1,8 @@
+//  TDD sample app MarvelBrowser by Jon Reid, http://qualitycoding.org/about/
+//  Copyright 2015 Jonathan M. Reid. See LICENSE.txt
+
+#import "QCOMarvelService.h"
+
+
+@implementation QCOMarvelService
+@end
