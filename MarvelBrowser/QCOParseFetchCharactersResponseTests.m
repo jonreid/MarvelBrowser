59831577fdffa517bdedbbbb9f40a270bcd654c4
Jon Reid <jon@qualitycoding.org>
2017-03-19 16:37:27 -0700

    Realize that parser should be standalone function separate from builder. Start moving tests to drive this

---
+++ MarvelBrowser/QCOParseFetchCharactersResponseTests.m
@@ -0,0 +1,73 @@
+#import "QCOParseFetchCharactersResponse.h"
+
+#import "QCOFetchCharactersResponseModel.h"
+
+#import <OCHamcrest/OCHamcrest.h>
+#import <XCTest/XCTest.h>
+
+
+@interface QCOParseFetchCharactersResponseTests : XCTestCase
+@end
+
+@implementation QCOParseFetchCharactersResponseTests
+
+- (NSData *)sampleResponse
+{
+    NSString *json =
+                    @"{"
+                    "  \"code\": 200,"
+                    "  \"status\": \"Ok\","
+                    "  \"data\": {"
+                    "    \"offset\": 1,"
+                    "    \"total\": 3,"
+                    "    \"results\": ["
+                    "      {"
+                    "        \"name\": \"NAME1\""
+                    "      },"
+                    "      {"
+                    "        \"name\": \"NAME2\""
+                    "      }"
+                    "    ]"
+                    "  }"
+                    "}";
+    return [json dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+- (void)testParseSampleResponse_ShouldHaveCode200
+{
+    QCOFetchCharactersResponseModel *response = QCOParseFetchCharactersResponse([self sampleResponse]);
+    
+    assertThat(@(response.code), is(@200));
+}
+
+//- (void)testParseSampleResponse_ShouldHaveStatusOk
+//{
+//    QCOFetchCharactersResponseModel *response = [self buildSampleResponse];
+//
+//    assertThat(response.status, is(@"Ok"));
+//}
+//
+//- (void)testParseSampleResponse_ShouldHaveOffset1
+//{
+//    QCOFetchCharactersResponseModel *response = [self buildSampleResponse];
+//
+//    assertThat(@(response.offset), is(@1));
+//}
+//
+//- (void)testParseSampleResponse_ShouldHaveTotal3
+//{
+//    QCOFetchCharactersResponseModel *response = [self buildSampleResponse];
+//
+//    assertThat(@(response.total), is(@3));
+//}
+//
+//- (void)testParseSampleResponse_ShouldHaveTwoCharacters
+//{
+//    QCOFetchCharactersResponseModel *response = [self buildSampleResponse];
+//
+//    assertThat(response.characters, containsIn(@[
+//            hasProperty(@"name", @"NAME1"),
+//            hasProperty(@"name", @"NAME2")]));
+//}
+
+@end
