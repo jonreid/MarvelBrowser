d30b001389e7ccaf4c281ee1db66d9d4312b80c4
Jon Reid <jon@qualitycoding.org>
2017-05-27 20:13:05 -0700

    Not compiling (failure): New test that task is retained

--- MarvelBrowser/QCONetworkRequestTests.m
+++ MarvelBrowser/QCONetworkRequestTests.m
@@ -1,5 +1,6 @@
 #import "QCONetworkRequest.h"
 
+#import <OCHamcrest/OCHamcrest.h>
 #import <OCMockito/OCMockito.h>
 #import <XCTest/XCTest.h>
 
@@ -17,6 +18,16 @@
     [sut startTask:mockTask];
     
     [verify(mockTask) resume];
+}
+
+- (void)testStartTask_ShouldRetainGivenTask
+{
+    id mockTask = mock([NSURLSessionTask class]);
+    QCONetworkRequest *sut = [[QCONetworkRequest alloc] init];
+    
+    [sut startTask:mockTask];
+    
+    assertThat(sut.currentTask, is(sameInstance(mockTask)));
 }
 
 @end
